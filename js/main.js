bf0c9fcce78759f74d0ee58db498a238a01a2dca
Albert E <vradnisntlong@gmail.com>
2022-10-29 02:16:04 +0700

    Added function to change next/prev sidebar tabs.

--- js/main.js
+++ js/main.js
@@ -24,3 +24,49 @@ contactsPillsTab.on('click', function(e) {
     e.preventDefault()
     $(this).tab('show')
 })
+
+navControls()
+
+function navControls() {
+    var i, items = $('.nav-link'), pane = $('.tab-pane')
+    // Next
+    $('.nextNav').on('click', function() {
+        for(i = 0; i < items.length; i++){
+            if($(items[i]).hasClass('active')){
+                break;
+            }
+        }
+        if(i < items.length - 1) {
+            // for tab
+            $(items[i]).removeClass('active').show(200)
+            $(items[i+1]).addClass('active').show(200)
+            // pane
+            $(pane[i]).show(200, function() {
+                $(pane[i]).removeClass('show active').removeAttr('style')
+            })
+            $(pane[i+1]).show(200, function() {
+                $(pane[i+1]).addClass('show active').removeAttr('style')
+            })
+        }
+    })
+    // Prev
+    $('.prevNav').on('click', function(){
+        for(i = 0; i < items.length; i++){
+            if($(items[i]).hasClass('active') == true){
+                break;
+            }
+        }
+        if(i != 0){
+            // for tab
+            $(items[i]).removeClass('active').show(200)
+            $(items[i-1]).addClass('active').show(200)
+            // for pane
+            $(pane[i]).show(200, function() {
+                $(pane[i]).removeClass('show active').removeAttr('style')
+            })
+            $(pane[i-1]).show(200, function() {
+                $(pane[i+1]).addClass('show active').removeAttr('style')
+            })
+        }
+    });
+}
