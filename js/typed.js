d86317e5eeccbecc48309f23435a6038893429e8
Albert E <vradnisntlong@gmail.com>
2022-10-30 01:33:50 +0700

    Added new function for typing in About Me page.

--- js/typed.js
+++ js/typed.js
@@ -1,6 +1,13 @@
 new Typed('#greet-text', {
     strings: ['Hi!', '你好!', 'Hola!', 'はじめまして!', 'Halo!', ' 안녕하세요!'],
-    typeSpeed: 200,
+    typeSpeed: 100,
     backSpeed: 100,
     loop: true
+})
+
+new Typed('#who-am-i-text', {
+    strings: ['Who Am I', 'About Me', 'Short Bio'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
 })
